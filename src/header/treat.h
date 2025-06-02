875eabb1407248ff35b474a98c4821c57d95c30d
Anisa Aulia Alhaqi <anisaalhaqi@gmail.com>
2025-06-02 21:38:59 +0700

    fix: penambahan parameter fungsi

--- src/header/treat.h
+++ src/header/treat.h
@@ -21,6 +21,6 @@
  * 
  * Return: `boolean` - True jika peresepan berhasil, false jika input tidak valid, pasien tidak ditemukan, belum didiagnosis, sudah diobati, atau dokter tidak berwenang.
  */
-boolean treatPatient(Hospital *hospital, Session *session);
+boolean treatPatient(Hospital *hospital, Session *session, const char *patientUsername);
 
 #endif // TREAT_H
